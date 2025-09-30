32217f862413b999f69b19b6ec114e420512d0ff
Mario Pella <pellamario18@gmail.com>
2025-09-30 23:17:47 +0200

    feat: Configurazione iniziale del progetto con Spring Boot

---
+++ src/main/java/com/mario/logistock/LogistockInventorySystemApplication.java
@@ -0,0 +1,13 @@
+package com.mario.logistock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogistockInventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogistockInventorySystemApplication.class, args);
+	}
+
+}
